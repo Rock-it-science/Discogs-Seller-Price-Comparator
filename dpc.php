0fe170e8b07f69b1d528e46ca1639128b6d264ad
Rock-it-science <wilmc17@gmail.com>
2019-03-01 10:56:35 -0800

    Finally figured out a way to display all want list items w/out error 429

--- dpc.php
+++ dpc.php
@@ -4,7 +4,17 @@
 require '../vendor/autoload.php';
 
 //Getting username passed from index page
-$username = $_REQUEST["username"];
+$username = $_REQUEST['username'];
+
+/*
+New method: getting all JSON data at once to avoid trying to do thousands of requests that either get throttled or blocked
+Or: Do all the requests once and save the data
+*/
+
+//Setting wantlist URL
+$wantURL = 'https://api.discogs.com/users/Rock_it_science/wants';
+//Setting seller inventory URL
+$sellerURL = 'https://api.discogs.com/users/bigschoolrecords/inventory?sort=item&sort_order=asc';
 
 //Setting up client with my user agent
 $client = Discogs\ClientFactory::factory([
@@ -15,11 +25,54 @@ $client = Discogs\ClientFactory::factory([
 //Throttle client
 $client->getHttpClient()->getEmitter()->attach(new Discogs\Subscriber\ThrottleSubscriber());
 
-$wantResponse = $client->getWantlist([
+//Wantlist client
+$wantClients = array();
+array_push($wantClients, $client->getWantlist([
     'username' => $username,
-    'page' => 3, //For example releases, use this page
+    'page' => 1, //For example releases, use this page
     'per_page' => 250
-]);
+]));
+//Create a client for every page
+//Find number of pages in wantList
+$pages = $wantClients[0]['pagination']['pages'];
+if($pages>2){
+  for($p=2; $p<=$pages; $p++){//Iterate through every page
+    array_push($wantClients, $client->getWantlist([
+        'username' => $username,
+        'page' => $p, //For example releases, use this page
+        'per_page' => 250
+    ]));
+  }
+}
+
+//Seller inventory client
+/*$invResponse = $client->getInventory([
+  'username' => $sellerArr[0],
+  'sort' => 'item',
+  'sort_order' => 'asc',
+  'per_page' => 250, //For now, only first 50 for example
+  'page' => 1 //Example releases are on this page
+]);*/
+
+$wantArray = array();
+?>
+<table>
+ <tr>
+   <th>Artist</th>
+   <th>Release Title</th>
+   <th>Seller</th>
+   <th>Price</th>
+ </tr>
+ <?php
+
+for($p=0; $p<sizeof($wantClients); $p++){//Iterate through every client (page)
+  foreach($wantClients[$p]['wants'] as &$item){//Iterating through items on page
+    echo '<tr><td></td><td>' . $item['basic_information']['title'] . '</td><td></td><td></td></tr>';
+  }
+}
+
+//ricbra API only method (429 error), have to iterate through every page
+/*
 // Loop through results adding the ID of each one to array wantsArr
 $wantsArr = array();
 
@@ -34,14 +87,7 @@ foreach ($wantResponse['wants'] as $result) {
 $sellerArr = array('bigschoolrecords');
 
 //Compare items to find all items that seller is selling that are on the buyer's wantlist
-//First get all items from seller's inventory
-$invResponse = $client->getInventory([
-  'username' => $sellerArr[0],
-  'sort' => 'item',
-  'sort_order' => 'asc',
-  'per_page' => 250, //For now, only first 50 for example
-  'page' => 1 //Example releases are on this page
-]);
+
 $sellItemArr = array();
 foreach($invResponse['listings'] as $item){
   $sellItemArr[] = $item['release']['id'];
@@ -60,17 +106,7 @@ foreach($wantsArr as $wantItemID){
 
 //Currently echos seller's item page and release page, which have seperate IDs
 //Need to figure out how to get release ID from seller's item page
-
- ?>
-<!--<table>
-  <tr>
-    <th>Artist</th>
-    <th>Release Title</th>
-    <th>Seller</th>
-    <th>Price</th>
-  </tr>-->
-  <?php
-  //Output prices compared to median prices in table
-   ?>
+*/
+?>
 </table>
 </body></html>
